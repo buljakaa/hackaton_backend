03ccf441682cbbdb692c12861ef8cde80b7024a8
Marko <marko.popovic@lightsoft.co.rs>
2021-04-22 18:59:47 +0200

    Radi register

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const logger = require('morgan');
 const db = require('./db');
+var bodyParser = require('body-parser')
 
 db.databaseInit();
 const http = require('http');
@@ -10,9 +11,11 @@ const User =require("./model/user");
 const users =require("./routes/user");
 const hostname = '127.0.0.1';
 const port = 3000;
+app.use(express.json());
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
--- routes/user.js
+++ routes/user.js
@@ -20,33 +20,33 @@ const User = require('../model/user');
 // "token":"123",
 // "gender":"M"
 // }
-router.post('/register', async(req, res) => {
-console.log(req.body);
+router.post('/register', (req, res) => {
+//console.log(req.body);  
    // res.status(201).json({message: 'User created', obj: "aa"}); 
     //const data = await User.find();
     //console.log(data);
-   // res.status(200).json({"data":data});
-    // var user = new User({
-    //     firstName: req.body.firstName,
-    //     lastName: req.body.lastName,
-    //     email: req.body.email,
-    //     username: req.body.username,
-    //     password: req.body.password,
-    //     phone: req.body.phone,
-    //     role: req.body.role,
-    //     verified: req.body.verified,
-    //     verificationToken: req.body.verificationToken,
-    //     token: req.body.token,
-    //     gender: req.body.gender,
-    // });
+//    res.status(200).json({"data":req.body});
+    var user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        phone: req.body.phone,
+        role: req.body.role,
+        verified: req.body.verified,
+        verificationToken: req.body.verificationToken,
+        token: req.body.token,
+        gender: req.body.gender,
+    }); 
 
-    
-    // user.save((err, result) => {
-    //     if (err) return res.status(500).json({title: 'An error occurred', error: err});
-    //     res.status(201).json({message: 'User created', obj: result});
+    console.log(user);
+    user.save((err, result) => {
+        if (err) return res.status(500).json({title: 'An error occurred', error: err});
+        res.status(201).json({message: 'User created', obj: user});
         
        
        
-    // });
+    });
 });
 module.exports = router;
