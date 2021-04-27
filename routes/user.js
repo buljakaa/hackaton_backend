362f68905b4e29c5bbd2294f20c9f56dd72e80e9
Marko <marko.popovic@lightsoft.co.rs>
2021-04-27 17:39:59 +0200

    ovo je sa loginom

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const nodemailer = require("nodemailer");
 
 // define express router
 const router = express.Router();
@@ -38,9 +38,6 @@ router.post('/register',async (req, res) => {
         password:await bcrypt.hash(req.body.password, 10),
         phone: req.body.phone,
         role: req.body.role,
-        verified: req.body.verified,
-        verificationToken: req.body.verificationToken,
-        token: req.body.token,
         gender: req.body.gender,
     }); 
 
@@ -49,13 +46,64 @@ router.post('/register',async (req, res) => {
         if (err) return res.status(500).json({title: 'An error occurred', error: err});
         res.status(201).json({message: 'User created', obj: user});
         
-       
+        sendEmail(user);
        
     });
 });
+//rtgaiuisufjwcoga
+function sendEmail (user) {
+    console.log(user.email);
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'markopoppriboj@gmail.com',
+          pass: 'rtgaiuisufjwcoga'
+        }
+      });
+      var mailOptions = {
+        from: 'markopoppriboj@gmail.com',
+        to: user.email,
+        subject: 'Welcome to book trading club!',
+        html: '<h1>Greeting message</h1><img src="http://www.off-the-recordmessaging.com/wp-content/uploads/2016/04/Thanks-For-Joining-Us1.jpg" /><p>We hope that you will enjoy in our site, find book that you looking for and sell some books too!</p>'
+      };
+     // proccess.env.MAIL_USERNAME = markopriboj@gmail.com
+    //   const info = await transporter.sendMail({
+    //     from: '"DIER APP" <' + process.env.MAIL_USERNAME + '>', // sender address
+    //     to: user.email, // list of receivers
+    //     subject: '[DIERS] Aktivirajte Vaš nalog', // Subject line
+    //     text: 'Aktiviraj nalog, link: ' + userLink, // plain text body
+    //     html: html, // html body
+    // });
+   
+     transporter.sendMail(mailOptions, (error, info) => {
+       if (error) console.log(error);
+       else console.log('Email sent: ' + info.response);
+     });
+}
 
-
-
+router.post('/login', (req, res) => {
+    
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (err) return res.status(500).json({title: 'An error occurred', error: err});
+        //no user with provided email address
+        if (!user) return res.status(401).json({
+            title: 'Login failed',
+            error: {message: 'Invalid email and/or password!'}
+        });
+        console.log(user.password);
+        //passwords does not maches
+        if (!bcrypt.compare(req.body.password, user.password)) return res.status(401).json({
+            title: 'Login failed', 
+            error: {message: 'Invalid email and/or password!'}
+        });
+        //user.token=token;
+        //login is success, create token for that u ser
+        const token = jwt.sign({user: user}, 'secret', {expiresIn: 10800});
+        user.token=token;
+        user.save();
+        res.status(200).json({message: 'Successfully logged in', token: token, userId: user._id});
+    }).select("+password");
+});
 
 module.exports = router;
  
