5fa289c026e056ecd825553ad815914c6e762ad4
Marko <marko.popovic@lightsoft.co.rs>
2021-05-12 10:44:49 +0200

    Random generate code

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -5,7 +5,7 @@ const authController = require('../controller/auth-controller');
 router.post('/register-user', authController.registerUser);
 router.post('/register-team', authController.registerTeam);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
+router.delete('/logout', authController.logout);
 router.get('/verify', authController.verify);
 
 module.exports = router;
